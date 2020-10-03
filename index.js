72f06025a19ab0674c76b93c468d3e518bd47df0
Nathan Rymarz <nrymarz@gmail.com>
2020-10-03 21:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === string)
+    return "I can't hear you!"
+  if (string.toUpperCase === string)
+    return "YES INDEED"
 }
